f4f99b16f9097ba94693f267eabb4e8a8f1887df
Roger <rogerio.reaper@gmail.com>
2020-09-11 10:55:34 -0300

    Recycler View criada

--- app/src/main/java/br/com/rogerioisj/pratica_cardview/MainActivity.java
+++ app/src/main/java/br/com/rogerioisj/pratica_cardview/MainActivity.java
@@ -1,14 +1,18 @@
 package br.com.rogerioisj.pratica_cardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mainRecycler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainRecycler = findViewById(R.id.main_recycler);
     }
 }
