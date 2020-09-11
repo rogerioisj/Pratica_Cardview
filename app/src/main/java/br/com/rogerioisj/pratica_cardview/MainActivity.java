f16becff6664a26a862bb328fcd083993bc19f26
Roger <rogerio.reaper@gmail.com>
2020-09-11 10:50:30 -0300

    First Commit

---
+++ app/src/main/java/br/com/rogerioisj/pratica_cardview/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.rogerioisj.pratica_cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
